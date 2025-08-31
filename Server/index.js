67de4b271de21405f1f3980abe7ce9aa311eaa23
Harshavardhan Channupati <charshavardhan122@gmail.com>
2025-08-31 17:34:17 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,8 @@ const Mongoose = require('mongoose');
 const {authRouter} = require('./Routes');
 const app = express();
 const Port = 5000;
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(cors());
 app.use(cookieParser()); 
 
